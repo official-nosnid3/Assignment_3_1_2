a60d058cb71dca1e2b42811c658d3acf66d7b42a
Edinson Cabral <Edinson.Cabral@mssa.newhorizons.com>
2024-11-18 12:44:37 -0600

    Simplified the IfYearIsLeap method

--- Program.cs
+++ Program.cs
@@ -60,10 +60,7 @@
 
             //return result;
 
-            if (
-                (year % 4 == 0 && year % 100 != 0) ||
-                (year % 100 == 0 && year % 400 == 0)
-                )
+            if ( (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0) )
                 return true;
             return false;
         }
